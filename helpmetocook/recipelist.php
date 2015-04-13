8b5757ffdf6de3151dfd02e95b587e67b2c2ecdb
NablaT <remi.pourtier@laposte.net>
2015-04-13 11:54:35 +0200

    Update xml and working on writing and reading xml file for ingredients

--- helpmetocook/recipelist.php
+++ helpmetocook/recipelist.php
@@ -9,13 +9,21 @@
  }
  
  function getBackIngredient($bdd){
-	 //include('../assets/ingredients.json');
-
-
-?>
-<script>
-
-</script>
-<?php	 
+	include('../assets/ingredients.php');
+	$lang="english"; //TOCHANGE
+	$ingredients= new SimpleXMLElement($xmlstr);
+	$ingredientlist=explode(" ",$_POST['textarea']);
+	print_r($ingredients);
+	echo(count($ingredients->ingredients));
+	for($i=0;$i<count($ingredientlist); $i++){
+		foreach($ingredients->ingredient as $ingredients->ingredient->{'Ban1'}){
+			$curIngredient=$ingredients->ingredients;
+			print_r($curIngredient);
+			echo($curIngredient);
+			echo(" bler");
+		}	
+	}
+	//print_r($ingredientlist);
+	//echo($ingredients->ingredients->{'Ban1'}->french);
  }
  ?>
