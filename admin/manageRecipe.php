0e7a226921ec0bb98aee52eda731d7e64bc22fd1
NablaT <remi.pourtier@laposte.net>
2015-04-13 13:19:00 +0200

    Css and text files for manage recipe frame

--- admin/manageRecipe.php
+++ admin/manageRecipe.php
@@ -2,35 +2,31 @@
 <html lang="en">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
-	<title> Menu des anciennes formulations</title>
-	<link rel="stylesheet" type="text/css" href="formu.css">
+	<title> <?php echo($title);?></title>
+	<link rel="stylesheet" type="text/css" href="../css/formu.css">
 
 </head>
 <body>
 <?php 
-include 'isadmin.php';
+include '../config/isadmin.php';
 
 if($isadmin){
 	print_page();
 }
 
 function print_page(){
+	include('../text/managerecipe_text.php');
 ?>
 
-<h2> Menu des anciennes formulations</h2>
+<h2> <?php echo($title);?></h2>
 
 <div id="wrapper">
- 
     <ul class="menu">
-        <li class="item1"><a href="add_old_formu.php">Ajouter une formulation</a></li>
-        <li class="item2"><a href="edit_old_formu.php">Editer une formulation </a></li>
-        <li class="item3"><a href="delete_old_formu.php">Supprimer une formulation</a></li>
-		<li class="item3"><a href="choice_list_oldformu.php">Liste des anciennes formulations</a></li>
-		<li class="item3"><a href="linknewold.php">Relier une nouvelle formulation à une ancienne</a></li>
-		<li class="item4"><a href="menu_admin.php"> Retour menu</a></li>
-
+        <li class="item1"><a href="add_old_formu.php"><?php echo($first);?></a></li>
+        <li class="item2"><a href="edit_old_formu.php"><?php echo($second);?></a></li>
+        <li class="item3"><a href="delete_old_formu.php"><?php echo($third);?></a></li>
+		<li class="item4"><a href="menu_admin.php"> <?php echo($home);?></a></li>
 	</ul>
- 
 </div>
 <?php
 }
---
+++ text/managerecipe_text.php
@@ -0,0 +1,8 @@
+ <?php
+ $title="Manage Recipe"; 
+ $first="Add a recipe";
+ $second="Edit a recipe";
+ $third="Delete recipe";
+ $home="Home";
+ 
+ ?>
