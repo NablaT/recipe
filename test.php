2770f518e29cda270bd10b2a320822eaa5232f21
NablaT <remi.pourtier@laposte.net>
2015-04-15 16:38:15 +0200

    Cleaning data base: put an unique id for every ingredient
    Some ingredients in database had the same ID. I did a method to give an
    unique id for every ingredient

--- test.php
+++ test.php
@@ -4,10 +4,61 @@ include('config/config.php');
 
 $file='D:\xampp\htdocs\recipe\text/spicies.txt';
 $banane="Banane"; 
+
+$listid=array();
+$listingredient=array();
+$reponse = $bdd->query('SELECT * FROM ingredient');
+// $donnes=$reponse->fetch();
+// print_r($donnes)
+while($donnes=$reponse->fetch()){
+	array_push($listid, $donnes['Idingredient']);
+	array_push($listingredient, $donnes['Name']);
+}
+$reponse->closeCursor();
+
+$sameid=array(); 
+$names=array();
+for($i=0; $i<count($listid);$i++){
+	for($j=0;$j<count($listid);$j++){
+		if($j!=$i){
+			if(strcmp($listid[$i],$listid[$j])==0){
+				array_push($sameid,$listid[$j]); 
+				array_push($names,$listingredient[$j]); 	
+			}
+		}
+	}
+}
+print_r($names);?> </br> <?php
+print_r($sameid);
+$currentId=$sameid[0];
+$cpt=1;
+for($i=0;$i<count($names);$i++){
+	if(strcmp($currentId,$sameid[$i])==0){
+		$cpt++;
+		$sameid[$i]=substr($sameid[$i],0,3)."".$cpt;
+	}
+	else{
+		$currentId=$sameid[$i];
+		$cpt=2;
+		$sameid[$i]=substr($sameid[$i],0,3)."".$cpt;
+	}
+}
+?>
+</br>
+<?php
+print_r($sameid);
+
+for($i=0;$i<count($names);$i++){
+	$reponse = $bdd->prepare('UPDATE ingredient SET Idingredient=:idingredient WHERE Name=:name');
+	$reponse->execute(array(
+		'idingredient' => $sameid[$i],
+		'name' => $names[$i]
+	)); 
+	$reponse->closeCursor();
+}
+/*
 if(is_file($file)){
 		if($tablines=file($file)){
-			echo(count($tablines));
-			print_r($tablines);
 			for($i=1;$i<count($tablines);$i++){		
 				$reponse = $bdd->prepare('UPDATE ingredient SET Description=:description WHERE Name=:name');
 				$reponse->execute(array(
@@ -22,7 +73,7 @@ if(is_file($file)){
 					'description' => "spicy",
 					'idingredient' => substr($tablines[$i],0,3)."1"
 				));
-				$req->closeCursor();*/
+				$req->closeCursor();
 			}
 		}
 		else{
