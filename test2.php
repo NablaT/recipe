e7ab821bae0bcb614d90bf0c43d763c3ee975e8d
NablaT <remi.pourtier@laposte.net>
2015-04-23 13:14:46 +0200

    Trying to correct a bugs on database

--- test2.php
+++ test2.php
@@ -1,9 +1,37 @@
 <?php
 include('config/config.php');
-
-$reponse=$bdd->prepare('DELETE FROM ingredient WHERE Name = ?');
-$reponse->execute(array("kiwii"));
+/*$savename=array();
+$savedescription=array();
+$saveid=array();
+$req=$bdd->query('SELECT * FROM ingredient');
+while($reponse=$req->fetch()){
+	array_push($savename,$reponse['Name']);
+	array_push($savedescription,$reponse['Description']);
+	array_push($saveid,$reponse['Idingredient']);
+}
+$reponse=$bdd->prepare('DELETE FROM ingredient WHERE Idingredient=?');
+$reponse->execute(array($saveid[0]));
 $reponse->closeCursor();
+$newname=trim($savename[0], " ");
+$newname=trim($newname, "\n");
+$req = $bdd->prepare('INSERT INTO ingredient(Name, Description, Idingredient) VALUES(:name, :description, :idingredient)');
+$req->execute(array(
+	'name' => $newname,
+	'description' => $savedescription[0],
+	'idingredient' => $saveid[0]
+));
+$req->closeCursor();
+*/
+$req=$bdd->prepare('SELECT * FROM ingredient WHERE Name=?');
+$req->execute(array("test"));
+$reponse=$req->fetch();
+print_r($reponse);
+$req->closeCursor();
+/*
+$req=$bdd->query('SELECT * FROM test WHERE Name=\' Kiwi\'');
+$reponse=$req->fetch();
+print_r($reponse);
+$req->closeCursor();
 /*
 $string="Kiwii";
 $id=getId($bdd,1,$string);
